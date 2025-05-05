69b68121b85f70e7d63f8462f672b86f00500b00
DeveloppeurDjango <mranebaye@email.com>
2025-05-05 13:18:25 +0000

    last last last

--- sondages/settings.py
+++ sondages/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['https://sondages-cq23.onrender.com']
+ALLOWED_HOSTS = ['sondages-cq23.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
